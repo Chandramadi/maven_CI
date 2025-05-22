49ec2a0e1d0442225aa9ca8081b60f914354228b
chandramadi <chandramadi6121@gmail.com>
2025-05-22 23:30:10 +0530

    Initial commit

---
+++ src/main/java/com/program6/maven/program6/App.java
@@ -0,0 +1,13 @@
+package com.program6.maven.program6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
